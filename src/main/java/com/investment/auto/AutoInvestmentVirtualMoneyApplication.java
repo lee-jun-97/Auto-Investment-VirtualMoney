44dd71fc3df9b47f2863aed703c62a1bbc2f57a6
LeeJun <yuog12@naver.com>
2022-08-18 00:08:44 +0900

    Inital Commit

---
+++ src/main/java/com/investment/auto/AutoInvestmentVirtualMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.investment.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInvestmentVirtualMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoInvestmentVirtualMoneyApplication.class, args);
+	}
+
+}
